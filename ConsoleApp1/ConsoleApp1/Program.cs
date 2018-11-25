28f7ec4fc84016a2c7562ca1506e0df85e9235c9
mohamaddawoodsalman <42866893+mohamaddawoodsalman@users.noreply.github.com>
2018-11-25 13:29:09 +0300

    myFirstProject
    my first project in program language c sharp

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            for (int i = 5; i > 0; i--)
+            {
+
+                Console.WriteLine();
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+
+                Console.WriteLine();
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
